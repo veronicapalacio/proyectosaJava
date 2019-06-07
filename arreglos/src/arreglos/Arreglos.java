77e5619f59172eb265371ef8eef657983cb246ee
veronicapalacio <51488378+veronicapalacio@users.noreply.github.com>
2019-06-07 12:30:29 -0500

    arreglosjava

---
+++ arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+import static java.time.Clock.system;
+import java.util.Scanner;
+
+/**
+ *
+ * @author vpala
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /*String b[] = new String [8];
+   
+        for (int i=0 ; i< 8; i++){
+            System.out.println("ingrese un caracter "+ (i+1));    
+            
+        Scanner a = new Scanner (System.in);
+        b[i]= a.nextLine(); 
+        
+        
+        }*/
+         
+        
+       int entero [] = new int [4];
+   
+        for (int i=0 ; i<= 3; i++){
+            System.out.println("ingrese un caracter "+ (i+1));    
+            
+        Scanner a = new Scanner (System.in);
+        entero [i]= a.nextInt(); 
+}
+    }
+        }
+
